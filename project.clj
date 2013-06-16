ad46be87fbb151ae1a81437671daec22c4934c9d
Linus Ericsson <oscarlinusericsson@gmail.com>
2013-06-16 15:53:27 +0200

    installed pedestal service part to the application - can show simple web pages

--- project.clj
+++ project.clj
@@ -1,12 +1,24 @@
 (defproject mekanikmaskin "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A way to learn basic university mechanics courses!"
+  :url "https://github.com/claj/mekanikmaskin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [ch.qos.logback/logback-classic "1.0.13"]
+                 [io.pedestal/pedestal.service "0.1.10-SNAPSHOT"]
+                 [io.pedestal/pedestal.jetty "0.1.10-SNAPSHOT"]
+                 [ns-tracker "0.2.1"]
+                 [ch.qos.logback/logback-classic "1.0.13" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.2"]
+                 [org.slf4j/jcl-over-slf4j "1.7.2"]
+                 [org.slf4j/log4j-over-slf4j "1.7.2"]
+
                  [com.datomic/datomic-free "0.8.4007" 
                   :exclusions [org.slf4j/slf4j-nop
                                org.slf4j/slf4j-log4j12]]] 
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :source-paths ["dev"]}}
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  :aliases {"run-dev" ["trampoline" "run" "-m" "dev"]}
+  :main ^{:skip-aot true} mekanikmaskin.server)
---
+++ src/mekanikmaskin/service.clj
@@ -0,0 +1,56 @@
+(ns mekanikmaskin.service
+  "pedestal service template
+
+this is where some magic happends (preferably as little as possible)"
+    (:require [io.pedestal.service.http :as bootstrap]
+              [io.pedestal.service.http.route :as route]
+              [io.pedestal.service.http.body-params :as body-params]
+              [io.pedestal.service.http.route.definition :refer [defroutes]]
+              [ring.util.response :as ring-resp]
+              [mekanikmaskin.logging :as log]))
+
+(defn about-page
+  [request]
+  (ring-resp/response (format "Clojure %s" (clojure-version))))
+
+(defn home-page
+  [request]
+  (log/info "a homepage visit!")
+  (ring-resp/response "Mekanikmaskinen!"))
+
+(defn login-page [request]
+  (log/info "a login-page visit!")
+  (ring-resp/response "Log in here!"))
+
+(defn login! [request]
+  (log/info "a login-post attempted")
+  (ring-resp/response "login confirmation placeholder"))
+
+(defn exercise [request]
+  (log/info "a request of an exercise")
+  (ring-resp/response "placeholder for your new exercise"))
+
+(defroutes routes
+  [[["/" {:get home-page}
+     ^:interceptors [(body-params/body-params) bootstrap/html-body]
+     ["/about" {:get about-page}]
+     ["/login" {:get login-page :post login!}]
+     ["/exercise" {:get exercise}]
+     ]]])
+
+;; You can use this fn or a per-request fn via io.pedestal.service.http.route/url-for
+(def url-for (route/url-for-routes routes))
+
+;; Consumed by mekanik-pedestal-service-test.server/create-server
+(def service {:env :prod
+              ;; You can bring your own non-default interceptors. Make
+              ;; sure you include routing and set it up right for
+              ;; dev-mode. If you do, many other keys for configuring
+              ;; default interceptors will be ignored.
+              ;; :bootstrap/interceptors []
+              ::bootstrap/routes routes
+              ;; Root for resource interceptor that is available by default.
+              ::bootstrap/resource-path "/public"
+              ;;::bootstrap/host "localhost"
+              ::bootstrap/type :jetty
+              ::bootstrap/port 8080})
