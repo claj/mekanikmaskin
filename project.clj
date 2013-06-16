cd50def4693efd0553cbeefc173a745fc225164b
Linus Ericsson <oscarlinusericsson@gmail.com>
2013-06-16 13:00:05 +0200

    datomic + logback dependencies added

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [ch.qos.logback/logback-classic "1.0.13"]
+                 [com.datomic/datomic-free "0.8.4007" 
+                  :exclusions [org.slf4j/slf4j-nop
+                               org.slf4j/slf4j-log4j12]]] 
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
